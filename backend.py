0c8ca0387fbce0f8968c2632c5d9ec243cc350da
ipzoone <saifalimusy@gmail.com>
2026-07-16 14:46:36 +0700

    raiway

--- backend.py
+++ backend.py
@@ -241,4 +241,5 @@ async def handle_chat(data: StrukturInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
